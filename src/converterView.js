46c51f94c789b395eae6c2984fb76842c518dca8
aerlevsedi <sobasmarta@gmail.com>
2022-10-29 22:55:34 +0200

    Add history list

--- src/converterView.js
+++ src/converterView.js
@@ -1,84 +1,110 @@
-import React, { useState, useEffect } from "react";
-import { View, TouchableOpacity, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, {useState, useEffect} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+} from 'react-native';
 // import { Button } from "@react-native-material/core";
 // import {Dropdown} from 'react-native-material-dropdown';
 import DropDownPicker from 'react-native-dropdown-picker';
 // import { useEffect } from "react/cjs/react.production.min";
 
 const ConverterView = () => {
-  const [justifyContent, setJustifyContent] = useState("space-evenly");
-  const [value1, setValue1] = useState("0");
-  const [value2, setValue2] = useState("0");
+  navigation = useNavigation();
+
+  const [conversionHistory, setConversionHistory] = useState([]);
+
+  const [justifyContent, setJustifyContent] = useState('space-evenly');
+  const [value1, setValue1] = useState('0');
+  const [value2, setValue2] = useState('0');
   const [switching, setSwitching] = useState(false);
   const [isFromImperialToMetric, setIsFromImperialToMetric] = useState(true);
 
   const [openImperialDropdown, setOpenImperialDropdown] = useState(false);
-  const [imperialUnit, setImperialUnit] = useState("inch");
+  const [imperialUnit, setImperialUnit] = useState('inch');
   const [imperialUnits, setImperialUnits] = useState([
-    { label: 'inch', value: 'inch' },
-    { label: 'foot', value: 'foot' },
-    { label: 'yard', value: 'yard' },
-    { label: 'pole', value: 'pole' },
-    { label: 'chain', value: 'chain' },
-    { label: 'furlong', value: 'furlong' },
-    { label: 'mile', value: 'mile' },
-    { label: 'league', value: 'league' }
+    {label: 'inch', value: 'inch'},
+    {label: 'foot', value: 'foot'},
+    {label: 'yard', value: 'yard'},
+    {label: 'pole', value: 'pole'},
+    {label: 'chain', value: 'chain'},
+    {label: 'furlong', value: 'furlong'},
+    {label: 'mile', value: 'mile'},
+    {label: 'league', value: 'league'},
   ]);
 
   const [openMetricDropdown, setOpenMetricDropdown] = useState(false);
-  const [metricUnit, setMetricUnit] = useState("m");
+  const [metricUnit, setMetricUnit] = useState('m');
   const [metricUnits, setMetricUnits] = useState([
-    { label: 'mm', value: 'mm' },
-    { label: 'cm', value: 'cm' },
-    { label: 'dm', value: 'dm' },
-    { label: 'm', value: 'm' },
-    { label: 'km', value: 'km' }
+    {label: 'mm', value: 'mm'},
+    {label: 'cm', value: 'cm'},
+    {label: 'dm', value: 'dm'},
+    {label: 'm', value: 'm'},
+    {label: 'km', value: 'km'},
   ]);
 
-  const [firstUnit, setFirstUnit] = useState("inch");
+  const [firstUnit, setFirstUnit] = useState('inch');
   const [firstUnits, setFirstUnits] = useState(imperialUnits);
 
-  const [secondUnit, setSecondUnit] = useState("m");
+  const [secondUnit, setSecondUnit] = useState('m');
   const [secondUnits, setSecondUnits] = useState(metricUnits);
 
   const impToMetric = {
-    "inch": 0.0254,
-    "foot": 0.3048,
-    "yard": 0.9144,
-    "pole": 5.0292,
-    "chain": 20.1168,
-    "furlong": 201.168,
-    "mile": 1609.344,
-    "league": 4828.032
-  }
+    inch: 0.0254,
+    foot: 0.3048,
+    yard: 0.9144,
+    pole: 5.0292,
+    chain: 20.1168,
+    furlong: 201.168,
+    mile: 1609.344,
+    league: 4828.032,
+  };
 
   const metrToMetric = {
-    "mm": 1000,
-    "cm": 100,
-    "dm": 10,
-    "m": 1,
-    "km": 0.001
-  }
+    mm: 1000,
+    cm: 100,
+    dm: 10,
+    m: 1,
+    km: 0.001,
+  };
 
   function convert() {
+    let conversion = '';
     if (isFromImperialToMetric === true) {
       let firstValue = parseFloat(value1);
-      let result = parseInt(firstValue * impToMetric[firstUnit] * metrToMetric[secondUnit] * 100) / 100.0;
+      let result =
+        parseInt(
+          firstValue * impToMetric[firstUnit] * metrToMetric[secondUnit] * 100,
+        ) / 100.0;
       setValue2(result.toString());
 
+      conversion =
+        firstValue + ' ' + firstUnit + ' = ' + result + ' ' + secondUnit;
     } else {
       let firstValue = parseFloat(value1);
-      let result = parseInt(firstValue / metrToMetric[metricUnit] / impToMetric[imperialUnit] * 100) / 100.0;
+      let result =
+        parseInt(
+          (firstValue / metrToMetric[metricUnit] / impToMetric[imperialUnit]) *
+            100,
+        ) / 100.0;
       setValue2(result.toString());
+      conversion =
+        firstValue + ' ' + metricUnit + ' = ' + result + ' ' + imperialUnit;
     }
+
+    setConversionHistory([...conversionHistory, conversion]);
+    console.log(conversionHistory);
   }
 
   useEffect(() => {
-    if (switching === false){
+    if (switching === false) {
       convert();
     }
   }, [value1, switching]);
-
 
   useEffect(() => {
     let tempUnit = firstUnit;
@@ -89,7 +115,6 @@ const ConverterView = () => {
 
       setSecondUnits(imperialUnits);
       setSecondUnit(tempUnit);
-
     } else {
       setFirstUnits(imperialUnits);
       setFirstUnit(secondUnit);
@@ -100,32 +125,28 @@ const ConverterView = () => {
     let temp = value1;
     setValue1(value2);
     setValue2(value1);
-    console.log({ isFromImperialToMetric });
+    console.log({isFromImperialToMetric});
   }, [isFromImperialToMetric]);
 
   return (
     <Layout
       label="justifyContent"
       selectedValue={justifyContent}
-      values={[
-        "space-evenly",
-      ]}
-      setSelectedValue={setJustifyContent}
-    >
-      <View
-        style={[
-          { flexDirection: 'row' }]}
-      >
-
+      values={['space-evenly']}
+      setSelectedValue={setJustifyContent}>
+      <View style={[{flexDirection: 'row'}]}>
         <TextInput
-          style={[{ backgroundColor: "black", width: 200 }]}
-          onChangeText={newText => { setSwitching(false), setValue1(newText)  }}
+          style={[{backgroundColor: 'white', width: 200}]}
+          onChangeText={newText => {
+            setSwitching(false), setValue1(newText);
+          }}
           value={value1}
           placeholder="Enter value"
           keyboardType="numeric"
         />
 
-        <DropDownPicker style={{ padding: 10, flex: 1, width: 150 }}
+        <DropDownPicker
+          style={{padding: 10, flex: 1, width: 150}}
           open={openImperialDropdown}
           value={firstUnit}
           items={firstUnits}
@@ -134,25 +155,23 @@ const ConverterView = () => {
           setItems={setFirstUnits}
           onSelectItem={convert}
         />
-
       </View>
-      <View
-        style={[{ backgroundColor: "skyblue" }]}
-      >
-        <Button title="Switch" onPress={() => setIsFromImperialToMetric(!isFromImperialToMetric)} />
+      <View style={[{backgroundColor: 'skyblue'}]}>
+        <Button
+          title="Switch"
+          onPress={() => setIsFromImperialToMetric(!isFromImperialToMetric)}
+        />
       </View>
-      <View
-        style={[
-          { flexDirection: 'row' }]}
-      >
+      <View style={[{flexDirection: 'row'}]}>
         <TextInput
-          style={[{ backgroundColor: "black", width: 200 }]}
+          style={[{backgroundColor: 'white', width: 200}]}
           value={value2}
           placeholder="Result value"
           keyboardType="numeric"
           editable={false}
         />
-        <DropDownPicker style={{ padding: 10, flex: 1, width: 150 }}
+        <DropDownPicker
+          style={{padding: 10, flex: 1, width: 150}}
           open={openMetricDropdown}
           value={secondUnit}
           items={secondUnits}
@@ -164,26 +183,27 @@ const ConverterView = () => {
       </View>
 
       <View>
-        <Button title="History" />
+        <Button
+          title="History"
+          onPress={() =>
+            navigation.navigate('HistoryView', {
+              conversionHistory: conversionHistory,
+            })
+          }
+        />
       </View>
-
     </Layout>
   );
 };
 
-const Layout = ({
-  label,
-  children,
-  values,
-  selectedValue,
-  setSelectedValue,
-}) => {
+const Layout = ({label, children, values, selectedValue, setSelectedValue}) => {
   return (
-    <View style={{ padding: 10, flex: 1 }}>
-      <View style={[styles.container, { [label]: selectedValue }]}>
+    <View style={{padding: 10, flex: 1}}>
+      <View style={[styles.container, {[label]: selectedValue}]}>
         {children}
       </View>
-    </View>);
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -196,34 +216,34 @@ const styles = StyleSheet.create({
     height: 50,
   },
   row: {
-    flexDirection: "row",
-    flexWrap: "wrap",
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
   button: {
     paddingHorizontal: 8,
     paddingVertical: 6,
     borderRadius: 4,
-    backgroundColor: "oldlace",
-    alignSelf: "flex-start",
-    marginHorizontal: "1%",
+    backgroundColor: 'oldlace',
+    alignSelf: 'flex-start',
+    marginHorizontal: '1%',
     marginBottom: 6,
-    minWidth: "48%",
-    textAlign: "center",
+    minWidth: '48%',
+    textAlign: 'center',
   },
   selected: {
-    backgroundColor: "coral",
+    backgroundColor: 'coral',
     borderWidth: 0,
   },
   buttonLabel: {
     fontSize: 12,
-    fontWeight: "500",
-    color: "coral",
+    fontWeight: '500',
+    color: 'coral',
   },
   selectedLabel: {
-    color: "white",
+    color: 'white',
   },
   label: {
-    textAlign: "center",
+    textAlign: 'center',
     marginBottom: 10,
     fontSize: 24,
   },
--- src/historyView.js
+++ src/historyView.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import {Text, View, Button} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {FlatList, StyleSheet} from 'react-native';
 
-const HistoryView = () => {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 12,
+    height: 44,
+    width: 300,
+  },
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    width: '100%',
+    marginVertical: 5,
+  },
+});
+
+const HistoryView = ({route, navigation}) => {
   navigation = useNavigation();
+
+  const {conversionHistory} = route.params;
+  console.log('HISTORY' + conversionHistory);
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <Text>History</Text>
-      <Button
-        title="Convert"
-        onPress={() =>
-          navigation.navigate('ConverterView')
-        }
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <FlatList
+        data={conversionHistory}
+        style={{paddingTop: 10}}
+        renderItem={({item}) => (
+          <View style={styles.card}>
+            <Text style={[styles.item]}>{item}</Text>
+          </View>
+        )}
       />
     </View>
   );
-}
+};
 
 export default HistoryView;
